ed201f2fff44e98d2e1944195a1c71ba99e90fef
parhamd <kingdeath1384@gmail.com>
2025-08-13 21:32:12 +0330

    First

---
+++ First session/First.sql
@@ -0,0 +1,13 @@
+﻿--درست کردن دیتا بیس
+CREATE DATABASE MyDatabase;
+
+
+--تغییر نام 
+alter database my_database
+   modify Name = myDatabase5
+
+
+--حذف کردن دیتابیس
+use master
+go
+DROP DATABASE IF EXISTS myDatabase5;
