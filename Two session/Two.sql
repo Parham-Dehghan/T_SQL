48d9b0dc1c8de526f67995b4d003a6c0868900e1
parhamd <kingdeath1384@gmail.com>
2025-08-14 12:36:57 +0330

    CREATE USER TABLE

--- Two session/Two.sql
+++ Two session/Two.sql
@@ -5,3 +5,12 @@ CREATE DATABASE test2; --ایجاد دیتا بیس
 go
 USE test2;-- فرا خوانی دیتا بیس
 go
+
+--جدول کاربران
+CREATE TABLE Users (
+   UserID INT IDENTITY(1,1)PRIMARY KEY,--شناسه یکتا
+   FirstName NVARCHAR(50) NOT NULL,--نام
+   LastName NVARCHAR(50) NOT NULL,--نام و نام حانوادگی
+   Email NVARCHAR(50) NOT NULL,--ایمیل
+   DataCreated DATETIME DEFAULT GETDATE()--تاریخ ایجاد
+);
