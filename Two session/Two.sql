5f175eaddbb25cb3324f2d13f7afa29c8d252314
parhamd <kingdeath1384@gmail.com>
2025-08-14 13:17:04 +0330

    CREATE TABLE Products and Orders

--- Two session/Two.sql
+++ Two session/Two.sql
@@ -14,3 +14,24 @@ CREATE TABLE Users (
    Email NVARCHAR(50) NOT NULL,--ایمیل
    DataCreated DATETIME DEFAULT GETDATE()--تاریخ ایجاد
 );
+
+--جدول محصولات
+CREATE TABLE Products (
+    ProductsID INT IDENTITY(1,1)PRIMARY KEY,--شناسه یکتا
+	ProductName NVARCHAR(100) NOT NULL,
+	Price DECIMAL(10,2) NOT NULL,
+	Stock INT DEFAULT 0,
+	DataAdded DATETIME DEFAULT GETDATE()
+);
+
+--جدول سفارشات
+CREATE TABLE Orders(
+    OrderID INT IDENTITY(1,1)PRIMARY KEY,--شناسه یکتا
+	UserID INT NOT NULL,
+	ProductID INT NOT NULL,
+	Quantity INT DEFAULT 1,
+	OrderData DATETIME DEFAULT GETDATE(),
+	FOREIGN KEY (UserID) REFERENCES Users(UserID),
+	FOREIGN KEY (ProductID) REFERENCES Products(ProductsID)
+);
+
