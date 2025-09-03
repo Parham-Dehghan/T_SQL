92794036180f2d28d6ce3abdd57aa13bf4221a5d
parhamd <kingdeath1384@gmail.com>
2025-09-03 15:44:29 +0330

    🔎 Step-by-step explanation: -- Start transaction BEGIN TRANSACTION;
    ✅ This marks the beginning of the transaction. From now on, all statements are executed as one logical unit. If something fails, we can roll everything back.
    
    UPDATE Accounts
    SET Balance = Balance - 200
    WHERE AccountID = 1;
    
    ✅ Deducts 200 from the account with AccountID = 1.
    
    UPDATE Accounts
    SET Balance = Balance + 200
    WHERE AccountID = 2;
    
    ✅ Adds 200 to the account with AccountID = 2.
    
    COMMIT; -- Save changes
    
    ✅ Finalizes and saves the transaction. After COMMIT, changes are permanent.

---
+++ eleven session/eleven.sql
@@ -0,0 +1,33 @@
+﻿use master
+go 
+
+create database eleven
+
+
+use eleven
+go
+
+CREATE TABLE Accounts (
+   AccountID INT PRIMARY KEY,
+   AccountHolder NVARCHAR(100),
+   Balance DECIMAL (18,2)
+);
+
+INSERT INTO Accounts (AccountID , AccountHolder, Balance)VALUES
+(1,'Ali', 1000.00),
+(2,'Sara',2000.00),
+(3,'Raza', 1500.00)
+
+
+--شروع یک Transaction
+BEGIN TRANSACTION;
+
+UPDATE Accounts
+SET Balance = Balance - 200
+WHERE AccountID = 1;
+
+UPDATE Accounts
+SET Balance = Balance + 200
+WHERE AccountID = 2;
+
+COMMIT;--ذخیره تغییرات
