e7af7d6c6a2956a47d0597f64eb4d13ad130abfe
parhamd <kingdeath1384@gmail.com>
2025-10-14 20:58:03 +0330

    Start Thirteenth

---
+++ Thirteenth session/Thirteenth.sql
@@ -0,0 +1,29 @@
+﻿use master
+go 
+
+create database Thirteenth
+
+use Thirteenth
+go
+
+CREATE TABLE Employees(
+    EmployeeID INT IDENTITY(1,1)
+PRIMARY KEY,
+    FirstName NVARCHAR(50),
+	LastName NVARCHAR(50),
+	DepartmentID INT,
+	Salary INT,
+);
+
+--درج داده ها 
+ INSERT INTO Employees(FirstName , LastName , DepartmentID , Salary)
+ VALUES
+ ('Ali','Ahmadi',1,12000),
+ ('Sara','Karimi',1,17000),
+ ('Reza','Mohammadi',2,14000),
+ ('Neda','Shirazi',2,18000),
+ ('Amir','Hosseini',3,9000),
+ ('Maryam','Rahimi',3,13000);
+
+
+
